f0d040f1ba32158c72f8dc47db7b532d9bda2c97
gregreust <greg.reust@gmail.com>
2022-09-28 08:16:03 -0500

    Added song serializer. Ready to add CRUD functions.

--- song/views.py
+++ song/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import SongSerializer
+from .models import Song
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_all_songs():
+
+    return Response
